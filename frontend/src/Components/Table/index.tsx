60173e6799779df4ee744a44527cfa5cade7f078
Dimitri Benz <dimitri.benz@jobmatch.me>
2023-09-22 19:06:36 +0200

    feat: styling

--- frontend/src/Components/Table/index.tsx
+++ frontend/src/Components/Table/index.tsx
@@ -1,37 +1,20 @@
-import React, { CSSProperties } from 'react'
+import React from 'react'
 
 interface TableProps<T> {
   data: T[]
   handleSort: (key: keyof T) => void
 }
 
-/** Table component */
+/**
+ * Table component
+ * Generate table headers and rows dynamically
+ * based on data keys
+ */
 function Table<T>(props: TableProps<T>) {
-
-  const tableStyle: CSSProperties = {
-    borderCollapse: 'collapse',
-    width: '100%',
-    border: '1px solid #ddd',
-  }
-
-  const thStyle: CSSProperties = {
-    padding: '8px',
-    textAlign: 'left',
-    border: '1px solid #ddd',
-  }
-
-  const tdStyle: CSSProperties = {
-    padding: '8px',
-    textAlign: 'left',
-    border: '1px solid #ddd',
-  }
-console.log("props", props)
-
-  return props.data?.length>0 ? (
-    <table style={tableStyle}>
-      <thead style={tableStyle}>
+  return props.data?.length > 0 ? (
+    <table>
+      <thead>
         <tr>
-          {/* Generate table headers dynamically based on data keys */}
           {Object.keys(props.data[0] as Record<string, any>).map(
             (key, index) => (
               <th key={index} onClick={() => props.handleSort(key as keyof T)}>
@@ -42,28 +25,30 @@ console.log("props", props)
         </tr>
       </thead>
       <tbody>
-        {/* Generate table rows dynamically based on sorted data */}
-        {props.data && props.data.map((item, index) => (
-          <tr key={index}>
-            {Object.entries(item as Record<string, any>).map(
-              ([key, value], i) => (
-                <td
-                  key={i}
-                  style={
-                    key === 'color'
-                      ? { backgroundColor: value, color: 'white' }
-                      : {}
-                  }
-                >
-                  {value}
-                </td>
-              )
-            )}
-          </tr>
-        ))}
+        {props.data &&
+          props.data.map((item, index) => (
+            <tr key={index}>
+              {Object.entries(item as Record<string, any>).map(
+                ([key, value], i) => (
+                  <td
+                    key={i}
+                    style={
+                      key === 'color'
+                        ? { backgroundColor: value, color: 'white' }
+                        : {}
+                    }
+                  >
+                    {value}
+                  </td>
+                )
+              )}
+            </tr>
+          ))}
       </tbody>
     </table>
-  ) : (<>Load data...</>)
+  ) : (
+    <>Load data...</>
+  )
 }
 
 export default Table
